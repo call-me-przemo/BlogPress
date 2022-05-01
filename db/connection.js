99ed952bd4695438ff74964f0248f9ac57eef166
Ernest Misha <pmiel13@gmail.com>
2022-05-01 15:09:01 +0200

    add auth features

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ import { join } from 'path';
 
 const file = await readFile(join(__dirname, 'db-config.yaml'));
 const { database, username, password, host, dialect} = await load(file);
-export const sequelize = new  Sequelize(database, username, password, {
+export const sequelize = new Sequelize(database, username, password, {
     host: host,
     dialect: dialect
 });
